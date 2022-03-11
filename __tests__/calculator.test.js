9a71da67492e6dc71f5a70f6c99f467721476c08
Kyle Miller <millerkyle85@gmail.com>
2022-03-10 22:39:53 -0800

    add second test fail

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -5,4 +5,9 @@ describe('Calc', () => {
     const userAge = new Calc(23, 100);
     expect(userAge.age).toEqual(23);
   });
+  test("should correctly determine age on Mercury when age on Earth is 23 years old", () => {
+    const userAge = new Calc(23);
+    userAge.ageOnPlanets();
+    expect(userAge.age).toEqual(95.8)
+  });
 });
