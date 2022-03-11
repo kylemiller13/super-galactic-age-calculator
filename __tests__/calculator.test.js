fdc9580b935b8649188a0eda477d5e61ef7f379f
Kyle Miller <millerkyle85@gmail.com>
2022-03-10 23:00:09 -0800

    Revert "third test failed"
    This reverts commit fc89b6ed4cffaf5dd759a3548b597faf6b1a585a.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -5,14 +5,9 @@ describe('Calc', () => {
     const userAge = new Calc(23, 100);
     expect(userAge.age).toEqual(23);
   });
-  test('should correctly determine age on Mercury when age on Earth is 23 years old', () => {
+  test("should correctly determine age on Mercury when age on Earth is 23 years old", () => {
     const userAge = new Calc(23);
     userAge.ageOnPlanets();
-    expect(userAge.mercuryAge).toEqual(95.8);
-  });
-  test('should correctly determine age on venus when age on Earth is 23 years old', () => {
-    const userAge = new Calc(23);
-    userAge.ageOnPlanets();
-    expect(userAge.venusAge).toEqual(37.0);
+    expect(userAge.mercuryAge).toEqual(95.8)
   });
 });
