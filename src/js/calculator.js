fa43ec1be1be42ae0be8d656864585f4cf5b6d09
Kyle Miller <millerkyle85@gmail.com>
2022-03-12 15:46:37 -0800

    test 8 pass

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -17,5 +17,6 @@ export class Calc {
   lifeExpectance(){
     this.mercuryExpectance = parseFloat((this.earthLifeExpectance / this.mercury).toFixed(1));
     this.venusExpectance = parseFloat((this.earthLifeExpectance / this.venus).toFixed(1));
+    this.marsExpectance = parseFloat((this.earthLifeExpectance / this.mars).toFixed(1));
   }
 }
